5c755241641badc8f768b4ef26401f2f86a6e97a
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-03-26 01:22:43 -0700

    landing page

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,15 +1,25 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 const Signin: React.FC = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+    <div className="flex flex-col items-center justify-center gap-8">
+      <p className="text-center text-2xl font-medium text-slate-400">
+        {sessionData && <span>👋 Welcome back, {sessionData.user?.name}!</span>}
       </p>
+      {sessionData && (
+        <Link
+          href={"/dashboard"}
+          className="rounded-full bg-gradient-to-t from-indigo-600 to-indigo-400 px-8 py-4 text-lg font-semibold text-slate-100 no-underline shadow-lg shadow-teal-400/40 transition hover:shadow-xl hover:shadow-teal-400/40 active:scale-95"
+        >
+          Dashboard
+        </Link>
+      )}
+
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:opacity-20"
+        className="rounded-full bg-gradient-to-t from-indigo-600 to-indigo-400 px-8 py-4 text-lg font-semibold text-slate-100 no-underline shadow-lg shadow-teal-400/40 transition hover:shadow-xl hover:shadow-teal-400/40 active:scale-95"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import Signin from "~/components/Signin";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   return (
@@ -11,14 +12,17 @@ const Home: NextPage = () => {
         <meta name="description" content="project management app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="min-h-screen bg-slate-200">
         {/**
          *  TODO:
          *  landing page
          *  authenticate dashboard page
          */}
-        <h1>Home Page</h1>
-        <Link href={"/dashboard"}>Dashboard</Link>
+        <div className="flex justify-center py-16">
+          <h1 className="bg-gradient-to-r from-indigo-500 to-teal-400 bg-clip-text text-9xl font-black text-transparent">
+            ProjecT3
+          </h1>
+        </div>
         <Signin />
       </main>
     </>
