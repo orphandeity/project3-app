e5f67edc068263f02bfe805374f4897286d71c9a
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-03-29 09:25:33 -0700

    update subtask query cache

--- src/components/SubtaskCard.tsx
+++ src/components/SubtaskCard.tsx
@@ -1,5 +1,4 @@
 import type { Subtask } from "@prisma/client";
-import { useCallback } from "react";
 import { api } from "~/utils/api";
 
 const SubtaskCard = ({
@@ -9,36 +8,41 @@ const SubtaskCard = ({
   taskId: string;
   subtask: Subtask;
 }) => {
+  // updater function for updating query cache after mutation
+  const updateFn = (oldData: Subtask[] | undefined) => {
+    const newData = oldData?.map((s) => {
+      if (s.id === subtask.id) return { ...s, isComplete: !s.isComplete };
+      else return s;
+    });
+    return newData;
+  };
+
   // api mutation: update subtask
   const utils = api.useContext();
   const { mutate: updateSubtask } = api.subtask.updateSubtask.useMutation({
     onSuccess() {
-      void utils.subtask.getSubtasks.invalidate({ taskId });
+      void utils.subtask.getSubtasks.setData({ taskId }, updateFn);
     },
   });
 
-  const handleUpdateSubtask = useCallback(() => {
+  const handleUpdateSubtask = () => {
     updateSubtask({
       subtaskId: subtask.id,
       subtaskStatus: !subtask.isComplete,
     });
-  }, [subtask.id, subtask.isComplete, updateSubtask]);
+  };
 
   return (
-    <li className="flex items-center gap-2 rounded-md bg-slate-100 p-2 shadow-inner dark:bg-slate-800">
+    <li className="flex cursor-grab items-center gap-2 rounded-md bg-slate-100 p-2 shadow-inner active:cursor-grabbing dark:bg-slate-800">
       <input
         type="checkbox"
-        id={subtask.id}
         checked={subtask.isComplete}
         onChange={handleUpdateSubtask}
         className="peer h-5 w-5 rounded border-slate-200 transition-colors checked:bg-indigo-400 hover:checked:bg-indigo-500 focus:ring-indigo-400 focus:ring-offset-slate-100 focus:checked:bg-indigo-400 dark:border-slate-600 dark:bg-slate-800 dark:checked:bg-indigo-500 dark:hover:checked:bg-indigo-600 dark:focus:ring-indigo-500 dark:focus:ring-offset-slate-800 dark:focus:checked:bg-indigo-500"
       />
-      <label
-        htmlFor={subtask.id}
-        className="text-sm font-normal text-slate-500 transition-colors peer-checked:text-slate-400 peer-checked:line-through dark:text-slate-400 dark:peer-checked:text-slate-500"
-      >
+      <p className="text-sm font-normal text-slate-500 transition-colors peer-checked:text-slate-400 peer-checked:line-through dark:text-slate-400 dark:peer-checked:text-slate-500">
         {subtask.title}
-      </label>
+      </p>
     </li>
   );
 };
--- src/components/SubtaskList.tsx
+++ src/components/SubtaskList.tsx
@@ -1,28 +1,22 @@
 import { api } from "~/utils/api";
 import SubtaskCard from "./SubtaskCard";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const SubtaskList = ({ taskId }: { taskId: string }) => {
-  const [parent, enableAnimations] = useAutoAnimate(/** optional config */);
-
   // api query: get subtasks
   const { data: subtasks } = api.subtask.getSubtasks.useQuery({ taskId });
 
-  if (!subtasks) return null;
-
   // subtask stats
-  const subtasksComplete = subtasks.filter(
+  const subtasksComplete = subtasks?.filter(
     (subtask) => subtask.isComplete
   ).length;
-  const subtasksTotal = subtasks.length;
 
   return (
     <div className="flex flex-col gap-1">
       <p className="text-xs font-bold text-slate-600 dark:text-slate-400">
-        Subtasks ({subtasksComplete} of {subtasksTotal})
+        Subtasks ({subtasksComplete} of {subtasks?.length})
       </p>
-      <ul ref={parent} className="flex flex-col gap-2">
-        {subtasks.map((s) => (
+      <ul className="flex flex-col gap-2">
+        {subtasks?.map((s) => (
           <SubtaskCard key={s.id} subtask={s} taskId={taskId} />
         ))}
       </ul>
