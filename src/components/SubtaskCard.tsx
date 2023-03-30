48b1bedaa94ab9d6831b7729c6bb6b359eec4f78
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-03-29 20:02:46 -0700

    ux: subtask cursor styles

--- src/components/SubtaskCard.tsx
+++ src/components/SubtaskCard.tsx
@@ -52,17 +52,17 @@ const SubtaskCard = ({
       style={style}
       {...attributes}
       id={subtask.id}
-      className="flex cursor-grab items-center gap-2 rounded-md bg-slate-100 p-2 shadow-inner active:cursor-grabbing dark:bg-slate-800"
+      className="flex cursor-default items-center gap-2 rounded-md bg-slate-100 p-2 shadow-inner dark:bg-slate-800"
     >
       <input
         type="checkbox"
         checked={subtask.isComplete}
         onChange={handleUpdateSubtask}
-        className="peer h-5 w-5 rounded border-slate-200 transition-colors checked:bg-indigo-400 hover:checked:bg-indigo-500 focus:ring-indigo-400 focus:ring-offset-slate-100 focus:checked:bg-indigo-400 dark:border-slate-600 dark:bg-slate-800 dark:checked:bg-indigo-500 dark:hover:checked:bg-indigo-600 dark:focus:ring-indigo-500 dark:focus:ring-offset-slate-800 dark:focus:checked:bg-indigo-500"
+        className="peer h-5 w-5 cursor-pointer rounded border-slate-200 transition-colors checked:bg-indigo-400 hover:checked:bg-indigo-500 focus:ring-indigo-400 focus:ring-offset-slate-100 focus:checked:bg-indigo-400 dark:border-slate-600 dark:bg-slate-800 dark:checked:bg-indigo-500 dark:hover:checked:bg-indigo-600 dark:focus:ring-indigo-500 dark:focus:ring-offset-slate-800 dark:focus:checked:bg-indigo-500"
       />
       <p
         {...listeners}
-        className="text-sm font-normal text-slate-500 transition-colors peer-checked:text-slate-400 peer-checked:line-through dark:text-slate-400 dark:peer-checked:text-slate-500"
+        className="cursor-grab text-sm font-normal text-slate-500 transition-colors active:cursor-grabbing peer-checked:text-slate-400 peer-checked:line-through dark:text-slate-400 dark:peer-checked:text-slate-500"
       >
         {subtask.title}
       </p>
